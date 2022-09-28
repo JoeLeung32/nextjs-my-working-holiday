b17f165bd5981e3621ef2e859bfc40356f0fb873
Joe.C.LEUNG <joe.c.leung@accenture.com>
2022-09-28 16:38:16 +0800

    Demo markdown list

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                <meta
+                    content='width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
+                    name='viewport' />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>About - MyWH</title>
+            </Head>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+
+export default About
